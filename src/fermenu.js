bb0286db187bd2961756391d8d61839115ea673b
fernando <79388875+Bot0ryan@users.noreply.github.com>
2021-02-23 00:59:13 -0300

    Update fermenu.js

--- src/fermenu.js
+++ src/fermenu.js
@@ -1,35 +1,27 @@
-const darkmenu = (prefix) => {
+const ryanmenu = (prefix) => {
 	return `
 
             COMANDOS:
 
 
-  *Comandos do Dark:*
+  *Comandos do Bot*
 
 ➸ *${prefix}loli*
 ➸ *${prefix}hentai*
 ➸ *${prefix}dono*
-➸ *${prefix}porno*
 ➸ *${prefix}boanoite*
 ➸ *${prefix}bomdia*
 ➸ *${prefix}boatarde*
-➸ *${prefix}mia*
-➸ *${prefix}mia1*
-➸ *${prefix}mia2*
-➸ *${prefix}belle*
-➸ *${prefix}belle1*
-➸ *${prefix}belle2*
-➸ *${prefix}belle3*
 ➸ *${prefix}ayeko*
 
 ╔════════════════════
-  TRADUZIDO POR *DARK*
+  *Jesus te ama❤️*
   DUVIDAS? 👇
-  WA.me/5517991134416
+  WA.me/554599271123
 ╚════════════════════`
 }
 
-exports.darkmenu = darkmenu
+exports.ryanmenu = ryanmenu
 
 
 
