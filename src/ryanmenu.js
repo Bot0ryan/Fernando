2f51480c1059701f8d688486c312a494af0cbfd7
fernando <79388875+Bot0ryan@users.noreply.github.com>
2021-02-23 01:00:15 -0300

    Create ryanmenu.js

---
+++ src/ryanmenu.js
@@ -0,0 +1,32 @@
+const ryanmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Bot*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  *Jesus te ama❤️*
+  DUVIDAS? 👇
+  WA.me/554599271123
+╚════════════════════`
+}
+
+exports.ryanmenu = ryanmenu
+
+
+
+
+
+
+
+
